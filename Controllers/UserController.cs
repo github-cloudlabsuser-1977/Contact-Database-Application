6a662ad8c7f2eba97c2ec2dea2232db3b37f94aa
github_cloudlabsuser_1977@cloudlabsaiuser.com <github_cloudlabsuser_1977@cloudlabsaiuser.com>
2024-06-10 11:11:36 +0000

    Some changes
    Added new packages, updated files for user handling and testing
    
    Several new packages have been added for testing, telemetry, and memory management. The project file has been updated with the removal and re-addition of `Microsoft.CodeDom.Providers.DotNetCompilerPlatform` package reference. A new file `test.cs` has been added for compilation, and new references have been added for the newly added packages. The `Web.config` and `packages.config` files have been updated to accommodate the new packages. `UserController.cs` has been improved for better user handling. `CRUD application 2.csproj` has been updated to import new props and targets files. Unit tests for `UserController` have been added in `test.cs`. `testEnvironments.json` has been updated with a new JSON object for testing version and environments, with WSL Ubuntu and Docker dotnet/sdk environments commented out.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -17,13 +17,15 @@ namespace CRUD_application_2.Controllers
             return View(userlist);
         }
  
-        // GET: User/Details/5
+      // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
-           // This method is responsible for displaying the details of a specific user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Details view.
-            return View(userlist.FirstOrDefault(x => x.Id == id));
+            var user = userlist.FirstOrDefault(x => x.Id == id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            return HttpNotFound();
         }
  
         // GET: User/Create
@@ -35,20 +37,17 @@ namespace CRUD_application_2.Controllers
             return View();
         }
  
-        // POST: User/Create
+      // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
-            // This method is responsible for handling the HTTP POST request to create a new user.
-            // It receives user input from the form submission and adds the new user to the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If an error occurs during the process, it returns the Create view to display any validation errors.
-            if (ModelState.IsValid)
+            if (user.Name!=null && user.Email!=null)
             {
+                // Add the new user to the userlist
                 userlist.Add(user);
                 return RedirectToAction("Index");
             }
+            // If there are validation errors, return the Create view with the user input
             return View(user);
         }
  
@@ -61,25 +60,26 @@ namespace CRUD_application_2.Controllers
             return View(userlist.FirstOrDefault(x => x.Id == id));
         }
  
-        // POST: User/Edit/5
+      // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
-            // Implement the Edit method (POST) here
-            // Find the user in the list
-           var userToUpdate = userlist.FirstOrDefault(x => x.Id == id);
-            if (userToUpdate != null)
+            var existingUser = userlist.FirstOrDefault(x => x.Id == id);
+            if (existingUser != null)
             {
-                // Update the user properties
-                userToUpdate.Name = user.Name;
-                userToUpdate.Email = user.Email;
-                return RedirectToAction("Index");
+                if (user.Name !=null && user.Email !=null)
+                {
+                    // Update the user's information
+                    existingUser.Name = user.Name;
+                    existingUser.Email = user.Email;
+   
+                    // Redirect to the Index action to display the updated list of users
+                    return RedirectToAction("Index");
+                }
+                // If there are validation errors, return the Edit view with the user input
+                return View(user);
             }
+            // If no user is found with the provided ID, return a HttpNotFoundResult
             return HttpNotFound();
         }
  
@@ -116,8 +116,7 @@ namespace CRUD_application_2.Controllers
         // Search a user from the list
         public ActionResult Search(string term)
         {
-           // search the user from the list then display the user matched
-           var searchResult = userlist.Where(x => x.Name.Contains(term) || x.Email.Contains(term)).ToList();
+            var searchResult = userlist.Where(x => x.Name.Contains(term) || x.Email.Contains(term)).ToList();
             return View("Index", searchResult);
         }
     }
